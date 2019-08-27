741bc730a95aeca7f1aaba271e2c631847f92ba3
Lance Chaney <lance.e.chaney@gmail.com>
2019-08-28 11:09:29 +1200

    Created serializable road network class

---
+++ Assets/Scripts/JsonTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var roadNetwork = new RoadNetwork();
+        roadNetwork.RoadSegments.Add(new RoadSegment(roadNetwork, 0, 1));
+        roadNetwork.RoadSegments.Add(new RoadSegment(roadNetwork, 1, 2));
+        roadNetwork.RoadVertices.Add(new RoadVertex(roadNetwork, 0, 0));
+        roadNetwork.RoadVertices.Add(new RoadVertex(roadNetwork, 50, 0));
+        roadNetwork.RoadVertices.Add(new RoadVertex(roadNetwork, 100, 0));
+        Debug.Log(JsonUtility.ToJson(roadNetwork, true));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoadNetwork.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class RoadNetwork
+{
+    public List<RoadSegment> RoadSegments { get => roadSegments; }
+    public List<RoadVertex> RoadVertices { get => roadVertices; }
+
+    [SerializeField]
+    private List<RoadSegment> roadSegments = new List<RoadSegment>();
+    [SerializeField]
+    private List<RoadVertex> roadVertices = new List<RoadVertex>();
+}
+
+[Serializable]
+public class RoadVertex
+{
+    public RoadVertex(RoadNetwork roadNetwork)
+    {
+        this.roadNetwork = roadNetwork;
+    }
+
+    public RoadVertex(RoadNetwork roadNetwork, Vector2 position)
+    {
+        this.roadNetwork = roadNetwork;
+        this.position = position;
+    }
+
+    public RoadVertex(RoadNetwork roadNetwork, float x, float y)
+        : this(roadNetwork, new Vector2(x, y))
+    { }
+
+    // Returns references to connected road segments
+    public List<RoadSegment> ConnectedSegments
+    {
+        get
+        {
+            List<RoadSegment> roadSegments = new List<RoadSegment>();
+            foreach (int roadSegmentIndex in ConnectedSegmentIndices)
+            {
+                roadSegments.Add(roadNetwork.RoadSegments[roadSegmentIndex]);
+            }
+            return roadSegments;
+        }
+    }
+
+    public List<int> ConnectedSegmentIndices { get => connectedSegmentIndices; }
+
+    public Vector2 position;
+
+    private List<int> connectedSegmentIndices = new List<int>();
+    private RoadNetwork roadNetwork;
+}
+
+[Serializable]
+public class RoadSegment
+{
+    public RoadSegment(RoadNetwork roadNetwork)
+    {
+        this.roadNetwork = roadNetwork;
+    }
+
+    public RoadSegment(RoadNetwork roadNetwork, int startVertIndex, int endVertIndex)
+    {
+        this.startVertIndex = startVertIndex;
+        this.endVertIndex = endVertIndex;
+        this.roadNetwork = roadNetwork;
+    }
+
+    // Returns a reference to the starting vertex of this road segment
+    public RoadVertex StartVertex
+    {
+        get => roadNetwork.RoadVertices[startVertIndex];
+    }
+
+    // Returns a reference to the end vertex of this road segment
+    public RoadVertex EndVertex
+    {
+        get => roadNetwork.RoadVertices[endVertIndex];
+    }
+
+    public int startVertIndex = -1;
+    public int endVertIndex  = -1;
+
+    private RoadNetwork roadNetwork;
+}
